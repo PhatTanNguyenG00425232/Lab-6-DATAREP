3caa962288d6d1190845ee1743bef6ac25aadd34
PhatTanNguyenG00425232 <G00425232@atu.ie>
2024-11-08 11:41:34 +0000

    exercise 2,3 completed

---
+++ BackEnd/server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const port = 4000;
+
+
+//get method listening to url, localhost
+//excute function when HTTP come in --> response
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
+
+//Get request to api/movies to return JSON data
+app.get('/api/movies', (req, res) => {
+    const movies = [
+        {
+            "Title": "Avengers: Infinity War",
+            "Year": "2018",
+            "imdbID": "tt4154756",
+            "Type": "movie",
+            "Poster": "https://example.com/poster1.jpg"
+        },
+        {
+            "Title": "Captain America: Civil War",
+            "Year": "2016",
+            "imdbID": "tt3498820",
+            "Type": "movie",
+            "Poster": "https://example.com/poster2.jpg"
+        },
+        {
+            "Title": "World War Z",
+            "Year": "2013",
+            "imdbID": "tt0816711",
+            "Type": "movie",
+            "Poster": "https://example.com/poster3.jpg"
+        }
+    ];
+    res.json({ movies });
+});
